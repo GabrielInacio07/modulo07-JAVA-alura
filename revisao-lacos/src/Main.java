fe4472165a0a68eed93db6ceb8ad15dd1c07815c
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-05 20:36:17 -0300

    Primeiro Desafio FOR I, com input

--- revisao-lacos/src/Main.java
+++ revisao-lacos/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
-        System.out.println("TESTE");
+        System.out.println("Desafio 01");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de degraus a subir");
+        int degraus = Integer.parseInt(input.nextLine());
+
+        for (int i = 1; i <= degraus; i++) {
+            System.out.println("Subindo o degrau: " + i);
+        }
+        System.out.println("Você chegou ao topo!");
     }
 }
