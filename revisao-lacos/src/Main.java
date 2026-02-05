b50c560c2991e7accc57a3fa4037ac20c03db131
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-05 20:56:35 -0300

    Terceiro Desafio FOR I, somar os valores PARES e IMPAR de um ARRAY,e verificar qual a diferença de valores

--- revisao-lacos/src/Main.java
+++ revisao-lacos/src/Main.java
@@ -1,17 +1,23 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Desafio 02");
+        System.out.println("Desafio 03");
         Scanner input = new Scanner(System.in);
-
-        int[] valores = {10,20,30,40,50};
-        int soma = 0;
-        for (int i = 0; i < valores.length; i++) {
-            soma += valores[i];
+        int somaPar = 0;
+        int somaImpar = 0;
+        for (int i = 0; i <= 100 ; i++) {
+            if(i % 2 == 0 ){
+                somaPar += i;
+            }else {
+                somaImpar += i;
+            }
         }
 
-        System.out.println("Valor da soma dos valores: " + soma);
+        System.out.println("A soma dos numeros pares é: " + somaPar);
+        System.out.println("Lista de numeros impares: " + somaImpar);
+        System.out.println("A diferença de valores é: " + (somaPar - somaImpar));
         input.close();
     }
 }
