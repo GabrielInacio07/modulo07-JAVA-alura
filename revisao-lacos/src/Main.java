e8c5ca0c32003361dcd0ccc97eb40fde227654e7
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-07 11:28:18 -0300

    Quinto Desafio declarar uma lista de Números e comparar qual dos elementos é o maior NUMBER

--- revisao-lacos/src/Main.java
+++ revisao-lacos/src/Main.java
@@ -1,19 +1,52 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Desafio 04");
+        System.out.println("Desafio 05");
         Scanner input = new Scanner(System.in);
-        int fatorial = 1;
+        ArrayList<Integer> arrayNumbers = new ArrayList<>();
 
-        System.out.println("Digite um número para saber seu fatorial: ");
-        int number = Integer.parseInt(input.nextLine());
+        int opcao = -1;
 
-        for (int i = number; i >= 1 ; i--) {
-            fatorial = fatorial * i;
+        while (opcao != 0){
+            System.out.println("Digite 1 - Para adicionar numero no ARRAY");
+            System.out.println("Digite 0 - Para Sair");
+
+            opcao = Integer.parseInt(input.nextLine());
+
+            if (opcao == 1){
+                try {
+                    System.out.println("Digite um numero");
+                    int number = Integer.parseInt(input.nextLine());
+                    arrayNumbers.add(number);
+                    System.out.println("Numero adicionado com sucesso!");
+                }catch (NumberFormatException  error){
+                    System.out.println("Valor Inválido. Digite apenas números! " + error.getMessage());
+                }
+
+            }else if(opcao == 0){
+                System.out.println("Encerrando operação...");
+                break;
+            }else{
+                throw new myException("Opção Inválida!!!");
+            }
         }
 
-        System.out.println("Fatorial de " + number + " = " + fatorial);
+        if (arrayNumbers.isEmpty()) {
+            System.out.println("A lista está vazia. Nenhum número para comparar.");
+        } else {
+            int maiorNumero = arrayNumbers.get(0);
+
+            for (int item : arrayNumbers) {
+                if (item > maiorNumero) {
+                    maiorNumero = item;
+                }
+            }
+            System.out.println("O maior número do Array é: " + maiorNumero);
+        }
+
+        input.close();
     }
 }
